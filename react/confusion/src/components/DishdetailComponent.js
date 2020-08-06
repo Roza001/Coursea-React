306748a2c756df3d25cd21c367375d7517e067ed
Roza001 <roza_1@op.pl>
2020-08-06 23:20:14 +0200

    functional components

--- react/confusion/src/components/DishdetailComponent.js
+++ react/confusion/src/components/DishdetailComponent.js
@@ -1,9 +1,7 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 
-class DishDetail extends Component {
-
-    renderComments(comments){
+    function RenderComments({comments}){
         if(comments != null){
             return comments.map(mycomment =>(
                 <ul key={mycomment.id} className="list-unstyled">
@@ -22,46 +20,42 @@ class DishDetail extends Component {
         }
     }
 
-    renderDish(dish){        
-        if(dish != null){
-            return (            
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
+    function RenderDish({dish}){        
+        return (            
+            <div className="col-12 col-md-5 m-1">
                     <Card>
                         <CardImg top src={dish.image} alt={dish.name} />
                         <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
                         </CardBody>
-                    </Card>
-                    </div>
-                        <div className="col-12 col-md-5 m-1">
+                    </Card>                        
+            </div>
+        );
+    }
+
+    const DishDetail = (props) => {    
+        if(props.dish != null){       
+            return(
+                <div className="container">
+                    <div className="row">
+                    <RenderDish dish={props.dish}/>
+                    <div className="col-12 col-md-5 m-1">
                         <h4>Comments</h4>
-                        {this.renderComments(dish.comments)}
+                        <RenderComments comments={props.dish.comments}/>
+                    </div>
+                    
                     </div>
                 </div>
             );
         }
         else{
-            return(
-                <div>             
-                </div>
-            )
+            return (
+                <div/>                
+            );
         }
-    }
-
-    render(){
-        const {dish} = this.props;            
-        
-        console.log('DishDetail Compenent render is invoked');
-        return(
-            <div className="container">
-                {this.renderDish(dish)}
-            </div>
-        );
         
     }
 
-}
 
 export default DishDetail
