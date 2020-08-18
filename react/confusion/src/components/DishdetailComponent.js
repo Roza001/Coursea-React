0c813be151214c7297b6a115fae70563ae84d6a3
Roza001 <roza_1@op.pl>
2020-08-18 14:24:56 +0200

    Redux Actions

--- react/confusion/src/components/DishdetailComponent.js
+++ react/confusion/src/components/DishdetailComponent.js
@@ -26,8 +26,8 @@ class CommentForm extends Component {
       }
 
     handleSubmit(values){
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
@@ -50,10 +50,10 @@ class CommentForm extends Component {
                             </Control.select>
                         </Col>
                     </Row>
-                    <Label htmlFor="yourName" md={12}>Your Name</Label>
+                    <Label htmlFor="author" md={12}>Your Name</Label>
                     <Row className="form-group">                        
                             <Col md={12}>
-                                <Control.text model=".yourName" id="yourName" name="yourName"
+                                <Control.text model=".author" id="author" name="author"
                                         placeholder="Your Name"
                                         className="form-control"
                                         validators={{
@@ -62,7 +62,7 @@ class CommentForm extends Component {
                                     />
                                 <Errors
                                         className="text-danger"
-                                        model=".yourName"
+                                        model=".author"
                                         show="touched"
                                         messages={{
                                             required: 'Required ',
@@ -80,7 +80,7 @@ class CommentForm extends Component {
                         </Row>
                         <Row className="form-group">
                             <Col md={{size:12}}>
-                                <Button type="submit" color="primary">
+                                <Button type="submit" value="submit" color="primary">
                                     Submit
                                 </Button>
                             </Col>
@@ -107,7 +107,7 @@ class CommentForm extends Component {
         );
     }
     
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, dishId}){
         if(comments != null){
             return (
             <div className="col-12 col-md-5 m-1">
@@ -122,7 +122,7 @@ class CommentForm extends Component {
                         );
                     })}
                 </ul>
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
             );
         }
@@ -154,7 +154,7 @@ class CommentForm extends Component {
                     </div>
                     <div className="row">
                         <RenderDish dish={props.dish}/>
-                        <RenderComments comments={props.comments}/>                                          
+                        <RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />                                          
                     </div>
                 </div>
             );
